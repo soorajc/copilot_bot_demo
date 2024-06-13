d799e5542a4564c82e47cb06fd234abe76f6ba34
Soorajc <soorajcmpm@gmail.com>
2024-06-13 17:32:03 +0530

    Updated: Aavatar card background

--- src/components/avatar_card/styles.ts
+++ src/components/avatar_card/styles.ts
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
         overflow: "hidden",
         borderWidth: 2,
         borderColor: "#ededed",
+        backgroundColor: '#ededed'
     },
     employeeName: {
         fontWeight: 'bold',
